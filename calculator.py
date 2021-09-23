fc8733d8b6754b410a35b1ff7a39e83be933bd3b
mcozar99 <m.cozar@alumnos.upm.es>
2021-09-23 13:03:36 +0100

    Debug

--- calculator.py
+++ calculator.py
@@ -1,75 +1,57 @@
 import pandas as pd
 import tabula
-import numpy as np
 import re
-import math
 
 # Choose PDF file with your expedient
 file_path = "M:/Universidad/GRADO/expediente.pdf"
 
+# Extracts the float if possible
+def convert_float(x):
+    if isinstance(x, float) == False:
+        if re.findall(r"[-+]?\d*\.\d+|\d+", x).__len__() != 0:
+            x = float('.'.join(re.findall(r"[-+]?\d*\.\d+|\d+", x)))
+    return x
+# Auxiliar function to filter strings from dataframe
+def sin_nota(x):
+    if isinstance(x, str):
+        return None
+    else:
+        return x
+
+def to_gpa(mark):
+    # Convert scale 0-10 to 0-4
+    if mark >= 9:
+        return 4
+    elif mark <= 9 and mark >= 7:
+        return 3
+    elif mark <= 7 and mark >= 5:
+        return 2
+    elif mark < 5:
+        return 1
+
 # Preparing the data
 df = tabula.read_pdf(file_path, pages='all')
+df = pd.concat(df)
+df = pd.DataFrame({'credits': df['Créditos'], 'mark' : df['Calificación']})
+# Drop nulls
+df = df.dropna()
+# Convert to numeric values
+df.credits = df.credits.apply(convert_float)
+df.mark = df.mark.apply(convert_float)
+# Total number of credit coursed
+creditos_totales = df.credits.values.sum()
+# Delete credits without mark
+df = df[df.mark == (df.mark.apply(sin_nota))]
+# GPA implementation
+df['gpa'] = df.mark.apply(to_gpa)
 
-credit_list = []
-grade_list = []
-for page in df:
-    credit_list.append(page['Créditos'].values)
-    grade_list.append(page['Calificación'].values)
-credit_list = list(np.concatenate(credit_list, axis=0))
-grade_list = list(np.concatenate(grade_list, axis=0))
-
-i = 0
-while i < (grade_list.__len__()):
-    if isinstance(grade_list[i], float) == False and grade_list[i] is not None:
-        if re.findall(r"[-+]?\d*\.\d+|\d+", grade_list[i]).__len__() != 0:
-            grade_list[i] = float('.'.join(re.findall(r"[-+]?\d*\.\d+|\d+", grade_list[i])))
-    if isinstance(credit_list[i], float) == False and credit_list[i] is not None:
-        if re.findall(r"[-+]?\d*\.\d+|\d+", credit_list[i]).__len__() != 0:
-            credit_list[i] = float('.'.join(re.findall(r"[-+]?\d*\.\d+|\d+", credit_list[i])))
-    i+=1
-
-for mark in grade_list:
-    if isinstance(mark, float) == False:
-        credit_list.pop(grade_list.index(mark))
-        grade_list.remove(mark)
-
-grade_list = [x for x in grade_list if pd.isnull(x) == False and x != 'nan']
-credit_list = [x for x in credit_list if pd.isnull(x) == False and x != 'nan']
-
-
-# Convert scale 0-10 to 0-4
-gpa_list = []
-for mark in grade_list:
-    if mark >= 9:
-        gpa_list.append(4)
-    elif mark <= 9 and mark >= 7:
-        gpa_list.append(3)
-    elif mark <= 7 and mark >= 5:
-        gpa_list.append(2)
-    elif mark < 5:
-        gpa_list.append(1)
-
-
-#Calculation of accumulative gpa and mean
-creditos_totales = sum(credit_list)
-media_acumulada = 0
-gpa_acumulado = 0
-i = 0
-while i < gpa_list.__len__():
-    gpa_acumulado += gpa_list[i]*credit_list[i]
-    media_acumulada += grade_list[i]*credit_list[i]
-    i+=1
-
-#Final calculation of parameters
-media_final = media_acumulada/creditos_totales
-gpa_final = gpa_acumulado/creditos_totales
-absolute_gpa = sum(gpa_list)/len(gpa_list)
-absolute_mean = sum(grade_list)/len(grade_list)
-
-
+# Final calculation
+gpa_final = (df.gpa * df.credits).sum() / df.credits.values.sum()
+media_final = (df.mark * df.credits).sum() / df.credits.values.sum()
+# Credits with Mark
+marked_credits = df.credits.values.sum()
 
 #Final display of results
+print('You coursed %s credits, %s of them have a mark'%(creditos_totales, marked_credits))
 print('Your Final ponderated GPA is : %s'%gpa_final)
-print('Your absolute GPA is %s'%absolute_gpa)
 print('Your Mean is %s'%media_final)
-print('Your absolute Mean is %s'%absolute_mean)
